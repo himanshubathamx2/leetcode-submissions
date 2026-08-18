a2b72b87ccf2967dc0b53f571b5be6b1e51dfa97
Himanshu Batham <112793814+himanshubathamx2@users.noreply.github.com>
2026-08-18 18:38:03 +0530

    Time: 3 ms (64.45%) | Memory: 46.8 MB (6.79%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -13,11 +13,11 @@ class Solution {
     }
 
     private boolean isSameTree(TreeNode root1, TreeNode root2) {
-        if (root1 == null && root2 == null) {
-            return true;
-        }
+        // if (root1 == null && root2 == null) {
+        //     return true;
+        // }
         if (root1 == null || root2 == null) {
-            return false;
+            return root1 == root2;
         }
         if (root1.val != root2.val) {
             return false;
