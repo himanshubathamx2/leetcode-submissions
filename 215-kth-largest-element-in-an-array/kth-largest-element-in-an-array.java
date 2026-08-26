1f0087086692c057c4a544841f4bb1298ac24fbc
Himanshu Batham <112793814+himanshubathamx2@users.noreply.github.com>
2026-08-26 20:16:53 +0530

    Time: 37 ms (75.64%) | Memory: 76 MB (21.11%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        int n = nums.length;
+        for(int i=n/2; i>=0; i--){
+            maxHeapify(nums, n, i);
+        }
+
+        for(int i=1; i<k; i++){
+            nums[0] = Integer.MIN_VALUE;
+            maxHeapify(nums, n, 0);
+        }
+        return nums[0];
+    }
+
+     public static void maxHeapify(int[] nums, int n, int i) {
+        int largest = i; // Initialize largest as root
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        // Check if left child is larger than root
+        if (left < n && nums[left] > nums[largest]) {
+            largest = left;
+        }
+
+        // Check if right child is larger than largest so far
+        if (right < n && nums[right] > nums[largest]) {
+            largest = right;
+        }
+
+        // If largest is not root
+        if (largest != i) {
+            // Swap
+            int temp = nums[i];
+            nums[i] = nums[largest];
+            nums[largest] = temp;
+
+            // Recursively heapify the affected sub-tree
+            maxHeapify(nums, n, largest);
+        }
+    }
+}
