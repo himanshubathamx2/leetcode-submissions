a0766de8712abe880db70352bbb0933cceecb28f
Himanshu Batham <112793814+himanshubathamx2@users.noreply.github.com>
2026-08-29 11:42:12 +0530

    Time: 2 ms (47.87%) | Memory: 52.5 MB (89.20%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -21,8 +21,7 @@ public class Solution {
                 return tmp1;
 
             tmp1 = tmp1.next;
-            tmp2 = tmp2.next;
-             
+            tmp2 = tmp2.next; 
            
         }
         return null;
