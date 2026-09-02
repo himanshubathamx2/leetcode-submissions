a3949e5f7722cf56d8ef061afef659010dae2fb0
Himanshu Batham <112793814+himanshubathamx2@users.noreply.github.com>
2026-09-02 19:46:26 +0530

    Time: 36 ms (13.71%) | Memory: 55.3 MB (22.70%) - LeetSync

--- 937-online-stock-span/online-stock-span.java
+++ 937-online-stock-span/online-stock-span.java
@@ -3,17 +3,12 @@ class StockSpanner {
     public StockSpanner() {
         stack = new Stack<>();
     }
-
     public int next(int price) {
-
         int span = 1;
-
         while (!stack.isEmpty() && stack.peek()[0] <= price) {
             span += stack.pop()[1];
         }
-
         stack.push(new int[]{price, span});
-
         return span;
     }
 }
