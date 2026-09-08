22f8015f827bce31347dfe8516638af35d738618
Himanshu Batham <112793814+himanshubathamx2@users.noreply.github.com>
2026-09-08 20:50:53 +0530

    Time: 32 ms (39.98%) | Memory: 146.7 MB (49.60%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -5,7 +5,7 @@ class Solution {
         int[] ans = new int[n-k+1];
         int j = 0;
         for(int i=0; i<n; i++){
-            if(!dq.isEmpty() && dq.peekFirst() <= i-k){
+            while(!dq.isEmpty() && dq.peekFirst() <= i-k){
                 dq.pollFirst();
             }   
             while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i]){
