a8104d5c46512d91e71e616155cfcfaa3653871f
Himanshu Batham <112793814+himanshubathamx2@users.noreply.github.com>
2026-09-14 20:53:23 +0530

    Time: 1 ms (87.18%) | Memory: 44.1 MB (56.84%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,24 +1,18 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        f(nums.length - 1, new ArrayList<>(), nums, ans);
+        f(0, new ArrayList<>(), nums, ans);
         return ans;
     }
 
-    public void f(int i, List<Integer> sub, int[] nums, List<List<Integer>> ans) {
-        if (i < 0) {
+    public void f(int i, List<Integer> sub, int[] nums, List<List<Integer>> ans){
+        if(i == nums.length){
             ans.add(new ArrayList<>(sub));
             return;
         }
-
-        // Take
         sub.add(nums[i]);
-        f(i - 1, sub, nums, ans);
-
-        // Backtrack
-        sub.remove(sub.size() - 1);
-
-        // Don't take
-        f(i - 1, sub, nums, ans);
+        f(i+1, sub, nums, ans);
+        sub.remove(sub.size()-1);
+        f(i+1, sub, nums, ans);
     }
 }
