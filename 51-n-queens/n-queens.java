8cf70eb23c0dc17c7e45426fd8de4f11a1698109
Himanshu Batham <112793814+himanshubathamx2@users.noreply.github.com>
2026-09-16 09:23:42 +0530

    Time: 1 ms (99.85%) | Memory: 46.1 MB (98.42%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,57 +1,44 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-       List<List<String>> ans = new ArrayList<>();
-       char[][] board = new char[n][n];
-       for(int i=0;i<n;i++){
+        List<List<String>> ans = new ArrayList<>();
+        boolean[] leftRow = new boolean[n];            // row usage
+        boolean[] lowerDiagonal = new boolean[n + n];  // row + col
+        boolean[] upperDiagonal = new boolean[n + n];  // (n - 1) + (col - row)
+
+        char[][] board = new char[n][n];
+        for(int i=0; i<board.length; i++)
             Arrays.fill(board[i], '.');
-       }
-       solve(0, board, ans , n);
-       return ans;
+        solve(0, n, board, ans, leftRow, lowerDiagonal, upperDiagonal);
+        return ans;
     }
-    
-    public void solve(int col, char[][] board, List<List<String>> ans, int n){
-        if(col==n){
+
+public void solve(int col, int n, char[][] board, List<List<String>> ans,
+                  boolean[] leftRow, boolean[] lowerDiagonal, boolean[] upperDiagonal) {
+        if(col == n){
             List<String> list = new ArrayList<>();
             for(char[] row : board){
                 String s = new String(row);
                 list.add(s);
             }
-            ans.add(new ArrayList<>(list));
+            ans.add(list);
             return;
         }
-        for(int row=0;row<n;row++){
-            if(safe(row, col, board, n)){
-                board[row][col]='Q';
-                solve(col+1, board, ans, n);
-                board[row][col] ='.';
-            }
-        }
-    }
+        
+        for(int row = 0; row < n; row++){
+            if (!leftRow[row] && !lowerDiagonal[row + col] && !upperDiagonal[n - 1 + col - row]) {
+                board[row][col] = 'Q';
+                leftRow[row] = true;
+                lowerDiagonal[row + col] = true;
+        upperDiagonal[n - 1 + col - row] = true;
 
-    public boolean safe(int row, int col, char[][] board, int n){
-        int dupRow = row;
-        int dupCol = col;
-        while(row>=0 && col>=0){
-            if(board[row][col]=='Q')
-                return false;
-            row--;
-            col--;    
+                solve(col + 1, n, board, ans, leftRow, lowerDiagonal, upperDiagonal);
+
+                board[row][col] = '.';
+                leftRow[row] = false;
+                lowerDiagonal[row + col] = false;
+                upperDiagonal[n - 1 + col - row] = false;
+}
+
         }
-        row = dupRow;
-        col = dupCol;
-        while(row<n && col>=0){
-            if(board[row][col]=='Q')
-                return false;
-            row++;
-            col--;    
-        }
-        row = dupRow;
-        col = dupCol;
-        while(col>=0){
-            if(board[row][col]=='Q')
-                return false;
-            col--;    
-        }
-        return true;
     }
 }
