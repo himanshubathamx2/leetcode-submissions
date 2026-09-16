83e9b367830a4863564954006d55cb02fe5857bd
Himanshu Batham <112793814+himanshubathamx2@users.noreply.github.com>
2026-09-16 14:18:21 +0530

    Time: 3 ms (14.18%) | Memory: 46.5 MB (5.12%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        f(0, ans, "", n, 0);
+        return ans;
+    }
+
+    public void f(int i, List<String> ans, String s, int n, int c){
+        if(c < 0)
+            return;
+        if(i == 2*n){
+            if(c == 0)
+                ans.add(s);
+            return;
+        }
+    
+        f(i+1, ans, s+'(', n, c+1);
+        if(c - 1 >= 0){   
+            f(i+1, ans, s+')', n, c-1);
+        }
+    }    
+}
